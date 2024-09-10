ede8ad6d1df3519c0fe3a57aea0c3da9aa0d5e70
kozzztia <kozzztia@gmail.com>
2024-09-10 12:55:25 +0300

    create banner bullets

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Project onWave initialized");
+
+const bunnerCards = document.querySelectorAll('.banner-card');
+const bunnerBullets = document.querySelectorAll('.banner-bullet');
+
+console.log(bunnerBullets , bunnerCards)
